f401a87c433ac53f620e6fbfb421c0fac39f31b1
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-06-12 16:44:10 +0300

    expereiment with command-line arguments

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    //commands 
+    //1.help
+    //2.create todo
+    //2.update todo
+    //3.list todo
+    //4.Delete todo
+    //5.
+    println!("{:?}", args);
 }
