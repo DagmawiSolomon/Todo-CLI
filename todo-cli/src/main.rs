ec30f0bceeca457cf49d3064f255194041c5a214
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-06-14 16:44:14 +0300

    try to fix bad choices

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -2,24 +2,24 @@ use colored::*;
 mod models;
 use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
-struct addflags{
-    due: String,
-    category: String,
-    proprity: String,
+// struct addflags{
+//     due: String,
+//     category: String,
+//     proprity: String,
 
-}
+// }
 #[derive(Debug, StructOpt)]
 struct Add{
     command: String,
     title: String,
-    flags: String,
+    
 
 }
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
     let task = models::Status{
         title:"hello".to_string(),
         
     };
-    let opts = Add::from_args();
-   println!("{:?}", opts);
+   println!("{}", task.title);
 }
