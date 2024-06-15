8c178e27957c1cd2d0c269df428311aafdbcdb82
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-06-15 07:08:57 +0300

    Implement the basic logic to parse positional arguments

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,25 +1,50 @@
 use colored::*;
-mod models;
 use structopt::StructOpt;
-#[derive(Debug, StructOpt)]
-// struct addflags{
-//     due: String,
-//     category: String,
-//     proprity: String,
 
-// }
 #[derive(Debug, StructOpt)]
-struct Add{
-    command: String,
-    title: String,
-    
-
-}
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let task = models::Status{
-        title:"hello".to_string(),
+enum Command {
+    add {
+        #[structopt(help = "Description of the task")]
+        task: String,
+        #[structopt(short, long, help = "Deadline of task")]
+        due: Option<String>,
+        #[structopt(short, long, help = "Level of priority: low, medium, high")]
+        priority: Option<String>,
+        #[structopt(short, long, help = "Category of task")]
+        category: Option<String>,
+        #[structopt(short, long, help = "Tags for task")]
+        tags: Vec<String>,
+    },
+    list {
+        #[structopt(short, long)]
+        due: Option<String>,
+        #[structopt(short, long)]
+        category: Option<String>,
+        #[structopt(short, long)]
+        tags: Vec<String>,
+        #[structopt(short, long)]
+        status: Option<String>,
+        #[structopt(short, long)]
+        priority: Option<String>,
+        // title - contains starts with ends with
+    },
+    sort{
+        #[structopt(short, long)]
+        due: Option<String>,
+        #[structopt(short, long)]
+        title: Option<String>,
         
-    };
-   println!("{}", task.title);
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name="Fundamentals", author = "Dagmawi Solomon", about= "I am a simple CLI to teach you the fundamentals")]
+struct Args {
+    #[structopt(subcommand)]
+    commands: Command,
+}
+
+fn main() {
+    let args = Args::from_args();
+    println!("{:?}", args);
 }
