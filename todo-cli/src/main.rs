ebec0ac7bd314a4886255cff5aec530704ee7bde
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-06-18 06:23:24 +0300

    Add: Implement a method to match passed arguments

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,8 +1,54 @@
+use rusqlite::{Connection, Result};
 use colored::*;
+use chrono::Utc;
 use structopt::StructOpt;
 mod args;
-
+mod models;
+mod database;
 fn main() {
+    database::create_tables();
     let args = args::Args::from_args();
-    println!("{:?}", args);
+    match args.commands {
+        args::Command::add {task,due,description,priority,category,emoji,tags} =>{
+            let x = models::Task{
+                title: task,
+                descripton: match description{
+                    None => " ".to_string(),
+                    Some(x) => x,
+                },
+                status: models::Status{
+                    title: "Pending".to_string(),
+                },
+                due_date: match due {
+                    None => " ".to_string(),
+                    Some(x) => x,
+                },
+                created_at: Utc::now(),
+                last_updated: Utc::now(),
+                prority: match priority {
+                    None => " ".to_string(),
+                    Some(x) => x,
+                },
+                category:models::Category{
+                    title: match category {
+                        None => " ".to_string(),
+                        Some(x) => x,
+                    },
+                    color: models::Color(0,0,0),
+                    emoji: match emoji{
+                        None => " ".to_string(),
+                        Some(x) => x,
+                    },
+                },
+                tags: models::Tag::new(tags), 
+            };
+            let x = database::add_task(x);
+            println!("{:?}", x);
+          
+        }
+        _ => println!("Mismatch")
+    }
+
+   
+
 }
