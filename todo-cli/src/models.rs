0f1d8e4e435bc7c7860a30bcd9c5afa76c9aa655
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-06-18 06:25:57 +0300

    Chore:

--- todo-cli/src/models.rs
+++ todo-cli/src/models.rs
@@ -1,28 +1,45 @@
+use chrono::{DateTime, Utc};
 
-pub struct Color(i64,i64,i64);
-
+#[derive(Debug)]
+pub struct Color(pub i64,pub i64, pub i64);
+#[derive(Debug)]
 pub struct Status{
     pub title:String,
     // color: Color
 }
 
+// pending inprogress Done 
+#[derive(Debug)]
 pub struct Category{
     pub title:String,
-    pub descripton:String,
     pub color: Color,
     pub emoji: String,
 }
-
+#[derive(Debug)]
 pub struct Tag{
     pub title: String,
     pub color: Color,
 }
 
-struct Task{
+impl Tag{
+    pub fn new(tags: Vec<String>) -> Vec<Tag>{
+        let mut list: Vec<Tag> = Vec::new();
+        for tag in tags{
+            list.push(Tag{
+                title: tag,
+                color: Color(0,0,0),
+            })
+        }
+        list
+    } 
+}
+
+#[derive(Debug)]
+pub struct Task{
     pub title: String,
     pub descripton: String,
-    pub created_at: String,
-    pub last_updated: String,
+    pub created_at: DateTime<Utc>,
+    pub last_updated: DateTime<Utc>,
     pub status: Status,
     pub prority: String,
     pub due_date: String,
