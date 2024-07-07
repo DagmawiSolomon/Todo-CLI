5a53ea28eb59bba70d95399d20233e85f3c5c7ff
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-07-07 08:32:17 +0300

    feat: experimental implementation of a macro

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -3,8 +3,27 @@ use structopt::StructOpt;
 mod args;
 mod models;
 mod database;
+
+use std::collections::HashMap;
+use macros::IntoStringHashMap;
+
+
+#[derive(IntoStringHashMap)]
+pub struct User {
+    username: String,
+    first_name: String,
+    last_name: String,
+}
 fn main() {
-   
+    let user = User {
+        username: "username".to_string(),
+        first_name: "First".to_string(),
+        last_name: "Last".to_string(),
+    };
+
+    let hash_map = HashMap::<String, String>::from(user);
+
+    dbg!(hash_map);
     let create_dbtables = database::create_tables();
     match create_dbtables {
     Err(err) => panic!("Error creating database: {}", err),
