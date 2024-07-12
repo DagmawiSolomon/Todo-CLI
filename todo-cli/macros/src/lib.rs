045111b902c4e6498ade23d361712a08b9fcc5b3
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-07-12 06:36:48 +0300

    fix: get the values and fields of the struct and print it

--- todo-cli/macros/src/lib.rs
+++ todo-cli/macros/src/lib.rs
@@ -9,11 +9,29 @@ use syn::{parse_macro_input, DeriveInput, Fields, Data};
 pub fn create_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let struct_identifier = &input.ident;
+    let fields = match &input.data {
+        Data::Struct(data) => &data.fields,
+        _ => panic!("Create can only be derived for structs"),
+    };
+  
+   let mut field_printing = Vec::new();
+
    
+   for field in fields {
+       let field_name = field.ident.as_ref().unwrap();
+       let print_code = quote! {
+           println!("Field '{}' has value '{:?}'", stringify!(#field_name), self.#field_name);
+       };
+       field_printing.push(print_code);
+   }
+
+        
+
+    
     let expanded = quote! {
         impl #struct_identifier{
             pub fn create(self) -> Self{
-                  println!("Create trait for struct {:?}", self);
+                #( #field_printing )*
 
                 self
             }
