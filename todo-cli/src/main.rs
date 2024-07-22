e9ff3165127c1c12afa15078280d767a8d20935c
dagmawiSolomon <02dagmawisolomon@gmail.com>
2024-07-22 18:32:25 +0300

    feat:give a more descriptive name to database now schema

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,69 +1,32 @@
 extern crate diesel;
 extern crate dotenvy;
 
-
-use chrono::Utc;
-use structopt::StructOpt;
-mod args;
-mod models;
-mod database;
-
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
 
-
+pub mod schema;
+pub mod models;
 
 fn main() {
+    dotenv().ok(); // Load the .env file
 
-    let create_dbtables = database::create_tables();
-    match create_dbtables {
-    Err(err) => panic!("Error creating database: {}", err),
-    _ => (),
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let connection = SqliteConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url));
+
+    // Example: Querying the database
+    use self::schema::users::dsl::*;
+
+    let results = users
+        .limit(5)
+        .load::<User>(&connection)
+        .expect("Error loading users");
+
+    println!("Displaying {} users", results.len());
+    for user in results {
+        println!("{}", user.name);
+        println!("----------\n");
+        println!("{}", user.email);
     }
-
-    let args = args::Args::from_args();
-    match args.commands {
-        args::Command::Add {task,due,description,priority,category,emoji,tags} =>{
-            let _x: models::Task = models::Task{
-                title: task,
-                description: match description{
-                    None => " ".to_string(),
-                    Some(x) => x,
-                },
-                status: models::Status{
-                    title: "Older".to_string(),
-                    color: "#323031".to_string(),
-                },
-                due_date: match due {
-                    None => " ".to_string(),
-                    Some(x) => x,
-                },
-                created_at: Utc::now().to_string(),
-                last_updated: Utc::now().to_string(),
-                prority: match priority {
-                    None => " ".to_string(),
-                    Some(x) => x,
-                },
-                category:models::Category{
-                    title: match category {
-                        None => " ".to_string(),
-                        Some(x) => x,
-                    },
-                    color: "#323031".to_string(),
-                    emoji: match emoji{
-                        None => " ".to_string(),
-                        Some(x) => x,
-                    },
-                },
-                tags: models::Tag::new(tags), 
-            };
-     
-          
-        }
-        _ => println!("Mismatch")
-    }
-
-   
-
 }
